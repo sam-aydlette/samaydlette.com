7e905ebf12dcfbe2d25e3b7486c739fdca94b399
sam-aydlette <sam.aydlette@gmail.com>
2025-01-30 15:58:20 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,12 @@ import { Navigation } from './components/Navigation.js';
 import { ImageHandler } from './components/ImageHandler.js';
 import { FormHandler } from './components/FormHandler.js';
 import { MobileMenu } from './components/mobile-menu.js';
-import { setupScrollObserver } from './utils/observers.js';
+//import { setupScrollObserver } from './utils/observers.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Navigation();
   new ImageHandler();
   new FormHandler();
   new MobileMenu();
-  setupScrollObserver();
+  //setupScrollObserver();
 });
