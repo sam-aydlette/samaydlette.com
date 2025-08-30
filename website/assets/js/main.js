057ced10be5338066747e1c0fc5bb765a4753b1d
sam-aydlette <sam.aydlette@gmail.com>
2025-08-29 22:20:43 -0400

    making the website more accessible

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -2,7 +2,6 @@ import { Navigation } from './components/Navigation.js';
 import { ImageHandler } from './components/ImageHandler.js';
 import { FormHandler } from './components/FormHandler.js';
 import { MobileMenu } from './components/mobile-menu.js';
-import { ThemeToggle } from './components/ThemeToggle.js';
 //import { setupScrollObserver } from './utils/observers.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,6 +9,5 @@ document.addEventListener('DOMContentLoaded', () => {
   new ImageHandler();
   new FormHandler();
   new MobileMenu();
-  new ThemeToggle();
   //setupScrollObserver();
 });
